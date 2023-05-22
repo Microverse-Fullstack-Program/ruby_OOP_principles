a37eadb740706a16fee6a8139da89f205caf58a5
cherelemma <chere.lemma@aastu.edu.et>
2023-05-22 15:12:11 +0300

    create copy of animal class

--- animal.rb
+++ animal.rb
@@ -1,78 +1,53 @@
 # Class definition
 class Animal
-end
-
-animal_1 = Animal.new
-animal_2 = Animal.new
-
-animal_1
-animal_2
-
-# Constructor and instance variables
-class Animal
-  def initialize
-    @id = Random.rand(1..1000)
-    @name = "Rex"
-    @number_of_legs = 4
-  end
-end
-
-animal_1 = Animal.new
-animal_2 = Animal.new
-
-animal_1
-animal_2
-
-# Constructor with parameters
-class Animal
-  def initialize(name, number_of_legs)
+  def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
-  end
-end
-
-animal_1 = Animal.new("Rex", 4)
-animal_2 = Animal.new("Bob", 8)
-
-animal_3 = Animal.new
-animal_4 = Animal.new("Rex")
-
-
-animal_1
-animal_2
-
-# Constructor with optional parameters
-class Animal
-  def initialize(number_of_legs, name = "Unknown")
-    @id = Random.rand(1..1000)
-    @name = name
-    @number_of_legs = number_of_legs
-  end
-end
-
-animal_1 = Animal.new(4, "Rex")
-animal_2 = Animal.new(8)
-
-
-animal_1
-animal_2
-
-# Method definition
-class Animal
-  def initialize(number_of_legs, name = "Unknown")
-    @id = Random.rand(1..1000)
-    @name = name
-    @number_of_legs = number_of_legs
+    @type = type
   end
 
+  # Encapsulation: binding data to method and 
+  # access or set value through setter and getter
+  # getter examples
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  # setter methods
+  def name=(value)
+    @name = value
+  end
+
+  # abstraction 
   def speak
-    "Bla bla bla"
+    "grrrr"
   end
 end
 
-animal_1 = Animal.new(4, "Rex")
-animal_2 = Animal.new(8)
+# object instansation
+animal_1 = Animal.new("dog", 4, "Rex")
 
-animal_1.speak
-animal_2.speak
+# access instance varible with getter
+animal_1.id
+animal_1.type
+animal_1.name
+animal_1.number_of_legs
+
+# another object instansation
+animal_2 = Animal.new("cat", 8)
+animal_2.name
+animal_2.name = "Fluffy" # set instance varibale with setter method
+animal_2.name
